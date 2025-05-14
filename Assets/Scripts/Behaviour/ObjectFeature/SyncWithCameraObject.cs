59ddca82ff4cf4dbbc3f8dc29f5a5f0f0c945138
umisoon@Desk <umisoon@desk.com>
2025-05-15 01:32:06 +0900

    [feature] カメラ方向のUIを追加

---
+++ Assets/Scripts/Behaviour/ObjectFeature/SyncWithCameraObject.cs
@@ -0,0 +1,16 @@
+﻿using Behaviour.Camera;
+using UnityEngine;
+
+namespace Behaviour.ObjectFeature
+{
+    public class SyncWithCameraObject : MonoBehaviour
+    {
+        [SerializeField]
+        private PlayerCam playerCam;
+        
+        private void Update()
+        {
+            transform.rotation = playerCam.transform.rotation;
+        }
+    }
+}
