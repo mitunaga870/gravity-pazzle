4fdd65f0e1bf245199c74e984c58c12ed389c730
umisoon@Desk <umisoon@desk.com>
2025-05-14 23:45:55 +0900

    [fix] カメラのピッチをクランプ

--- Assets/Scripts/Behaviour/Camera/PlayerCam.cs
+++ Assets/Scripts/Behaviour/Camera/PlayerCam.cs
@@ -12,13 +12,14 @@ namespace Behaviour.Camera
     {
         private const float Threshold = 0.01f;
         private const float Sensitivity = 5f;
-        private const float xOffset = 5f;
-        private const float yOffset = 2f;
+        private const float MaxPitch = 85f;
         
         private Transform? _playerTrans = null;
         private GravType? _gravType = null;
         
         private Vector3 _prevPos = Vector3.zero;
+        
+        private float _pitch = 0f;
         
         private void Awake()
         {
@@ -35,39 +36,60 @@ namespace Behaviour.Camera
             
             // プレイヤーの位置と重力の種類を取得
             var playerPos = _playerTrans.position;
-            var playerPosOffset = playerPos + new Vector3(xOffset, yOffset, 0);
             var gravType = _gravType.Value;
             var playerTrans = _playerTrans;
             
             // マウスの動きに合わせてカメラを回転させる
-            var mouseX = Input.GetAxis("Mouse X");
-            var mouseY = Input.GetAxis("Mouse Y");
+            var mouseX = Mathf.Clamp(
+                Input.GetAxis("Mouse X") * Sensitivity * -1,
+                MaxPitch*-1,
+                MaxPitch
+                );
+            var mouseY = Mathf.Clamp(
+                Input.GetAxis("Mouse Y") * Sensitivity * -1,
+                MaxPitch*-1,
+                MaxPitch
+                );
             
             // 閾値以上の動きがあった場合にカメラを回転させる
-            if (Mathf.Abs(mouseX) > Threshold)
+            if (Mathf.Abs(mouseY) > Threshold)
             {
                 // 軸をカメラの回転角分だけ回転させる
                 var rotatedAxis = GravUtils.GetGravRightUnit(gravType,transform);
+                
+                // ピッチを積算
+                var unClampedPitch = _pitch + mouseY;
+                // ピッチを制限
+                _pitch = Mathf.Clamp(unClampedPitch, MaxPitch * -1, MaxPitch);
+                // 超えた場合、その分を引く
+                var excessPitch = unClampedPitch - _pitch;
+                // 変化量計算
+                var deltaPitch = mouseY - excessPitch;
+
+                Debug.Log( $"deltaPitch: {deltaPitch}, " +
+                          $"unClampedPitch: {unClampedPitch}, " +
+                          $"excessPitch: {excessPitch}, " +
+                          $"pitch: {_pitch}");
+
+
                 // カメラの回転を更新
                 transform.RotateAround(
                     playerPos,
                     rotatedAxis,
-                    mouseY * Sensitivity * -1
+                    deltaPitch
                 );
-                
-                //　回転軸を表示
-                Debug.DrawRay(playerPos, rotatedAxis, Color.red);
             }
 
-            if (Mathf.Abs(mouseY) > Threshold)
+            if (Mathf.Abs(mouseX) > Threshold)
+            {
+                
                 // カメラの回転を更新
                 transform.RotateAround(
                     playerPos,
                     GravUtils.GetGravDirectionUnit(gravType),
-                    mouseX * Sensitivity * -1
+                    mouseX
                 );
-
-            Debug.Log(transform.rotation);
+            }
         }
         
         /**
