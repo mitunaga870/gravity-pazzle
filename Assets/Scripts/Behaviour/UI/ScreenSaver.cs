b79cd1ad0a15331b4539782c5f75e547e7df09a3
mitunaga870 <af21022@shibaura-it.ac.jp>
2025-08-01 03:05:27 +0900

    [change] プレイ開始時にスクリーンセーバーを表示するように

--- Assets/Scripts/Behaviour/UI/ScreenSaver.cs
+++ Assets/Scripts/Behaviour/UI/ScreenSaver.cs
@@ -25,8 +25,8 @@ namespace Behaviour.UI
 
         #region Private Fields
 
-        // 放置時間
-        private float _idleTime;
+        // 放置時間　最初はスクリーンセーバーを表示するために最大値
+        private float _idleTime = float.MaxValue;
 
         // スクリーンセーバーが表示されているかどうか
         private bool _isScreenSaverActive;
@@ -68,10 +68,9 @@ namespace Behaviour.UI
             }
             else
             {
-                _idleTime += Time.deltaTime; // 放置時間を更新
-
-                // スクリーンセーバーの時間を超えたら表示
+                // 放置時間がスクリーンセーバーの時間を超えた場合はスクリーンセーバーを表示
                 if (_idleTime >= screenSaverTime) ShowScreenSaver();
+                else _idleTime += Time.deltaTime; // 放置時間を更新
             }
         }
 
