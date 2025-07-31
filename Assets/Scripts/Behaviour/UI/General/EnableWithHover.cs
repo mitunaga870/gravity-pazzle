201eec1a224ba87cc9d8bdb03f0231b5856e90f8
mitunaga870 <af21022@shibaura-it.ac.jp>
2025-08-01 02:35:45 +0900

    [add] ホバー時アイコン追加

---
+++ Assets/Scripts/Behaviour/UI/General/EnableWithHover.cs
@@ -0,0 +1,36 @@
+﻿#region
+
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+#endregion
+
+namespace Behaviour.UI.General
+{
+    /// <summary>
+    ///     UIにホバーした際に対象を有効化するコンポーネント
+    /// </summary>
+    public class EnableWithHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+    {
+        // ホバーした際に有効化する対象のリスト
+        [SerializeField]
+        private List<GameObject> targets = new();
+
+        /// <summary>
+        ///     ホバーした際に有効化する対象を有効化する
+        /// </summary>
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            foreach (var target in targets) target.SetActive(true);
+        }
+
+        /// <summary>
+        ///     ホバーが外れた際に対象を無効化する
+        /// </summary>
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            foreach (var target in targets) target.SetActive(false);
+        }
+    }
+}
