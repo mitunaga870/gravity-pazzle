fd3225870199ad6ef993ad812143c4cc638bd92b
mitunaga870 <af21022@shibaura-it.ac.jp>
2025-08-23 01:58:33 +0900

    [add] 入力コントローラー利用用インターフェース

---
+++ Assets/Scripts/Behaviour/ObjectFeature/interface/InputBase.cs
@@ -0,0 +1,15 @@
+﻿#region
+
+using Behaviour.Controller.General;
+using UnityEngine;
+
+#endregion
+
+namespace Behaviour.ObjectFeature.@interface
+{
+    public interface IInputBase
+    {
+        [SerializeField]
+        InputController Input { get; set; }
+    }
+}
