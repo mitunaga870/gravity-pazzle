09da85387ba2c7c7ce30b50d9d1dfd31b29a3165
mitunaga870 <af21022@shibaura-it.ac.jp>
2025-08-23 01:56:48 +0900

    [feature] 入力取得用コントローラー兼Sceneステータス管理機能追加

---
+++ Assets/Scripts/Behaviour/Controller/General/InputController.cs
@@ -0,0 +1,66 @@
+﻿#region
+
+using System.Collections.Generic;
+using Lib.State.Scene;
+using UnityEngine;
+
+#endregion
+
+namespace Behaviour.Controller.General
+{
+    /// <summary>
+    ///     ユーザー入力を処理するためのコントローラー
+    ///     ゲーム状況とかでキー判定するために処理を集約する。
+    ///     状態操作も必須とする
+    /// </summary>
+    [RequireComponent(typeof(SceneStateController))]
+    public class InputController : MonoBehaviour
+    {
+        /// <summary>
+        ///     キー入力を取得する。
+        /// </summary>
+        /// <param name="key">チェックするキー</param>
+        /// <param name="targets">入力を許す状態配列</param>
+        /// <param name="context">状態コンテキスト</param>
+        /// <returns>キーが押されているかどうか</returns>
+        public bool GetKey(KeyCode key, IEnumerable<ISceneState> targets, SceneStateContext context)
+        {
+            // 現在の状態が許可された状態のいずれかであるかをチェック
+            // ReSharper disable once ConvertIfStatementToReturnStatement
+            if (!CheckState(targets, context)) return false;
+
+            // キー入力をチェック
+            return Input.GetKey(key);
+        }
+
+        /// <summary>
+        ///     キー入力を取得する。
+        /// </summary>
+        /// <param name="key">チェックするキー</param>
+        /// <param name="targets">入力を許す状態配列</param>
+        /// <param name="context">状態コンテキスト</param>
+        /// <returns>キーが押された瞬間かどうか</returns>
+        public bool GetKeyDown(KeyCode key, IEnumerable<ISceneState> targets, SceneStateContext context)
+        {
+            // 現在の状態が許可された状態のいずれかであるかをチェック
+            // ReSharper disable once ConvertIfStatementToReturnStatement
+            if (!CheckState(targets, context)) return false;
+
+            // キー入力をチェック
+            return Input.GetKeyDown(key);
+        }
+
+        private bool CheckState(IEnumerable<ISceneState> targets, SceneStateContext context)
+        {
+            // 現在の状態がnullの場合は、全ての状態で許可する
+            if (context?.CurrentState == null) return true;
+
+            // 現在の状態が許可された状態のいずれかであるかをチェック
+            foreach (var target in targets)
+                if (context.CurrentState.GetType() == target.GetType())
+                    return true;
+
+            return false;
+        }
+    }
+}
---
+++ Assets/Scripts/Lib/State/Scene/ISceneState.cs
@@ -0,0 +1,36 @@
+﻿namespace Lib.State.Scene
+{
+    /// <summary>
+    ///     Sceneの状態を表すインターフェース
+    /// </summary>
+    public interface ISceneState
+    {
+        /// <summary>
+        ///     ステート名
+        /// </summary>
+        SceneState StateName { get; }
+
+        /// <summary>
+        ///     状態を変更する
+        /// </summary>
+        /// <param name="next">次の状態</param>
+        /// <param name="forceChange">強制的に変更するか</param>
+        /// <returns>変更が成功したか</returns>
+        bool Change(ISceneState next, bool forceChange = false);
+
+        /// <summary>
+        ///     状態に入るときの処理
+        /// </summary>
+        void OnEnter(ISceneState prev = null);
+
+        /// <summary>
+        ///     状態から出るときの処理
+        /// </summary>
+        void OnExit();
+    }
+
+    public enum SceneState
+    {
+        InGame
+    }
+}
