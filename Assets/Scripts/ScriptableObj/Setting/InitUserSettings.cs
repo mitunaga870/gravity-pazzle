0fbd8dcf54889fb43a36cd6f0c88cc76de0dd2f3
mitunaga870 <af21022@shibaura-it.ac.jp>
2025-10-09 02:07:32 +0900

    [fix] スクリプタブルオブジェクトのアセットメニューパス修正

--- Assets/Scripts/ScriptableObj/Setting/InitUserSettings.cs
+++ Assets/Scripts/ScriptableObj/Setting/InitUserSettings.cs
@@ -10,7 +10,7 @@ namespace ScriptableObj
     /// <summary>
     ///     初期ユーザー設定を保存するスクリプタブルオブジェクト
     /// </summary>
-    [CreateAssetMenu(fileName = "初期ユーザー設定", menuName = "ScriptableObj/初期ユーザー設定", order = 0)]
+    [CreateAssetMenu(fileName = "初期ユーザー設定", menuName = "ScriptableObj/設定/初期ユーザー設定", order = 0)]
     public class InitUserSettings : ScriptableObject
     {
         #region Serialized Fields
