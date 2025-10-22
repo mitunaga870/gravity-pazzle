942d89ce287a740140a596054eec0a54d56a2510
mitunaga870 <af21022@shibaura-it.ac.jp>
2025-10-23 00:59:45 +0900

    [update] 反作用を起こさないオブジェクトをプレイヤーだけから任意に変更できるように

---
+++ Assets/Scripts/Behaviour/ObjectFeature/ContactModify/IgnoreNonRiactiveCollision.cs
@@ -0,0 +1,33 @@
+﻿#region
+
+#endregion
+
+#region
+
+using UnityEngine;
+
+#endregion
+
+namespace Behaviour.ObjectFeature.ContactModify
+{
+    /// <summary>
+    /// 反作用を起こさないオブジェクトを無視するコンタクトモディファイア
+    /// </summary>
+    public class IgnoreNonRiactiveCollision : ANonReactiveContactModifier
+    {
+        private int _bodyId;
+        protected override int BodyId => _bodyId;
+
+        private void Start()
+        {
+            var rb = GetComponent<Rigidbody>();
+            _bodyId = rb.GetInstanceID();
+        }
+
+        protected override bool IsIgnore(int selfId, int otherId, bool isSelfNonReactive, bool isOtherNonReactive)
+        {
+            return (isOtherNonReactive && selfId == BodyId) ||
+                   (isSelfNonReactive && otherId == BodyId);
+        }
+    }
+}
---
+++ Assets/Scripts/Behaviour/ObjectFeature/ContactModify/OnlyNonRectiveCollision.cs
@@ -0,0 +1,39 @@
+﻿#region
+
+using UnityEngine;
+
+#endregion
+
+namespace Behaviour.ObjectFeature.ContactModify
+{
+    /// <summary>
+    /// 反作用を起こさないオブジェクトのためのKinetic Rigidbody用コンタクトモディファイア
+    /// </summary>
+    [RequireComponent(typeof(Collider))]
+    [RequireComponent(typeof(Rigidbody))]
+    public class OnlyNonRectiveCollision : ANonReactiveContactModifier
+    {
+        private int _bodyId;
+        protected override int BodyId => _bodyId;
+
+        private void Start()
+        {
+            var rb = GetComponent<Rigidbody>();
+            _bodyId = rb.GetInstanceID();
+        }
+
+        protected override bool IsIgnore(int selfId, int otherId, bool isSelfNonReactive, bool isOtherNonReactive)
+        {
+            var isSelf = selfId == BodyId;
+            var isOther = otherId == BodyId;
+
+            // 自分が含まれているか
+            var isContainSelf = isSelf || isOther;
+
+            // 自分が含まれず、かつどちらかが非反作用オブジェクトの場合は無視する
+            var isIgnore = isContainSelf && !(isSelfNonReactive || isOtherNonReactive);
+
+            return isIgnore;
+        }
+    }
+}
