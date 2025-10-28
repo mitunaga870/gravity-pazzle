e5e553a79eb238c792f067980170148c302101d3
ユーザー名は任意 <127680896+sirofyu@users.noreply.github.com>
2025-10-29 01:04:05 +0900

    プロップスと便利コード作成

---
+++ Assets/FBX/Model/Props/PathAttribute.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+/// <summary>
+/// ドラッグアンドドロップでパスを設定するための属性
+/// 参考：https://kan-kikuchi.hatenablog.com/entry/PathAttribute_1
+/// </summary>
+public class PathAttribute : PropertyAttribute
+{
+}
