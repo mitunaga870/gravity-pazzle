3ec5219e7716718b4b9623afc08b674995031e61
mitunaga870 <af21022@shibaura-it.ac.jp>
2025-10-28 16:15:00 +0900

    [fix] リセット時の移動をカメラの追従移動に含めないよう修正

--- Assets/Scripts/Behaviour/ObjectFeature/ResetableObject.cs
+++ Assets/Scripts/Behaviour/ObjectFeature/ResetableObject.cs
@@ -1,6 +1,6 @@
 ﻿#region
 
-using System;
+using Behaviour.Camera;
 using Behaviour.Gravity;
 using Lib.Logic;
 using UnityEngine;
@@ -15,54 +15,91 @@ namespace Behaviour.ObjectFeature
     public class ResetableObject : MonoBehaviour
     {
         private Vector3 _initialPosition;
-        private void Start()
+
+        private bool _hasRigidbody;
+        private Rigidbody _rigidbody;
+
+        private bool _hasGravBehaviour;
+        private VGravBehaviour _gravBehaviour;
+
+        private bool _hasPlayerCam;
+        private PlayerCam _playerCam;
+
+        // リセットのための一時状況を解除させるための遅延時間
+        private const float ResetDelay = 0.1f;
+
+        private void Awake()
         {
-            // 初期位置を覚える
+            // 初期位置と必要なコンポーネントをキャッシュする
             _initialPosition = transform.position;
+
+            _rigidbody = GetComponent<Rigidbody>();
+            _hasRigidbody = _rigidbody != null;
+
+            _gravBehaviour = GetComponent<VGravBehaviour>();
+            _hasGravBehaviour = _gravBehaviour != null;
+
+            _playerCam = GetComponent<PlayerCam>();
+            _hasPlayerCam = _playerCam != null;
         }
 
         /// <summary>
         ///     位置をリセットする
         /// </summary>
-        [Obsolete("Obsolete")]
         public void ResetPosition()
         {
-            // inActiveならアクティブにする
-            if (!gameObject.activeSelf)
-                gameObject.SetActive(true);
-            
-            // 速度をリセット
-            var rb = GetComponent<Rigidbody>();
-            if (rb != null)
-            {
-                // Rigidbodyがある場合
-                // 一時的に位置固定
-                rb.isKinematic = true;
-                // 速度をリセット
-                rb.velocity = Vector3.zero;
-                rb.angularVelocity = Vector3.zero;
-                
-                // RigidbodyのisKinematicをfalseに戻す
-                var delay = GeneralUtils.DelayCoroutine(
-                    0.1f,
-                    () =>
-                    {
-                        rb.isKinematic = false;
-                    });
-                StartCoroutine(delay);
-            }
-            
-            // 重力を戻す
-            var grav = GetComponent<VGravBehaviour>();
-            if (grav != null)
-            {
-                // GravBehaviourがある場合
-                // 重力を初期値に戻す
-                grav.SetGravAffected(grav.InitialGravType, forceChange: true, registerOperation: false);
-            }
-            
-            // 初期位置に戻す
+            EnsureGameObjectIsActive();
+            ResetRigidbodyState();
+            ResetGravityState();
+            ResetPlayerCamState();
+
+            // 物理演算が位置変更に干渉しないように、Rigidbodyをkinematicにした後で位置をリセットすることが重要
             transform.position = _initialPosition;
         }
+
+        #region Private Methods
+
+        private void EnsureGameObjectIsActive()
+        {
+            if (!gameObject.activeSelf)
+            {
+                gameObject.SetActive(true);
+            }
+        }
+
+        private void ResetRigidbodyState()
+        {
+            if (!_hasRigidbody) return;
+
+            // 位置をクリーンにリセットし、既存の動きを停止させるために、一時的にRigidbodyをkinematicにする
+            _rigidbody.isKinematic = true;
+            _rigidbody.linearVelocity = Vector3.zero;
+            _rigidbody.angularVelocity = Vector3.zero;
+
+            // 少し遅延させてからRigidbodyのkinematicを解除する
+            var coroutine =
+                GeneralUtils.DelayCoroutine(ResetDelay, () => { _rigidbody.isKinematic = false; });
+            StartCoroutine(coroutine);
+        }
+
+
+        private void ResetGravityState()
+        {
+            if (!_hasGravBehaviour) return;
+
+            // オブジェクトの重力を初期設定に戻す
+            _gravBehaviour.SetGravAffected(_gravBehaviour.InitialGravType, true, false);
+        }
+
+        private void ResetPlayerCamState()
+        {
+            if (!_hasPlayerCam) return;
+
+            // リセット時の移動量を移動しないようにする
+            var currentPos = transform.position;
+            _playerCam.OffsetNextFollow += _initialPosition - currentPos;
+        }
+
+        #endregion
     }
 }
