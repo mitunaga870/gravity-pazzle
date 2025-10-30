04ccdbb7b9331245ec4c8012552be54f2f1b7e71
mitunaga870 <af21022@shibaura-it.ac.jp>
2025-10-30 19:45:49 +0900

    [fix] スクリプト場所修正

--- Assets/FBX/Model/Props/PathAttribute.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-/// <summary>
-/// ドラッグアンドドロップでパスを設定するための属性
-/// 参考：https://kan-kikuchi.hatenablog.com/entry/PathAttribute_1
-/// </summary>
-public class PathAttribute : PropertyAttribute
-{
-}
