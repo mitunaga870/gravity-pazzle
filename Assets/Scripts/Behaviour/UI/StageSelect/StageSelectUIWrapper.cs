d42c5e7d9cce61b369e5a68d23cb179c7ec08668
mitunaga870 <af21022@shibaura-it.ac.jp>
2025-10-31 23:33:32 +0900

    [feature] stageセレクト実装

---
+++ Assets/Scripts/Behaviour/UI/StageSelect/StageSelectUIWrapper.cs
@@ -0,0 +1,48 @@
+﻿#region
+
+using Behaviour.Controller.General.DontDestoroy;
+using TMPro;
+using UnityEngine;
+
+#endregion
+
+namespace Behaviour.UI.StageSelect
+{
+    /// <summary>
+    ///     stageセレクトUIの配置や表示を管理するラッパークラス
+    /// </summary>
+    public class StageSelectUIWrapper : MonoBehaviour
+    {
+        #region Serialized Fields
+
+        [SerializeField]
+        private StageSelectButton stageSelectButtonPrefab; // ステージセレクトUIのプレハブ
+
+        [SerializeField]
+        private Transform buttonContainer; // ステージセレクトUIの配置先コンテ
+
+        [SerializeField]
+        private TMP_Text hoverStageNameText; // ホバー中のステージ名を表示するテキスト
+
+        [SerializeField]
+        private TMP_Text hoverStageTitleText; // ホバー中のステージ説明を表示
+
+        #endregion
+
+
+        private void Start()
+        {
+            // stageを取得し、ステージセレクトUIを生成・配置する
+            var environmentSetting = SettingDataController.Instance.EnvironmentSetting;
+            var stages = environmentSetting.Stages;
+
+            for (var i = 0; i < stages.Count; i++)
+            {
+                var stage = stages[i];
+
+                var stageSelectButton = Instantiate(stageSelectButtonPrefab, buttonContainer);
+                stageSelectButton.Initialize(stage, i + 1, hoverStageNameText, hoverStageTitleText);
+            }
+        }
+    }
+}
