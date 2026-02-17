1e886be0f5806c2fb5707469ddd4c33bdfe18b31
mitunaga870 <af21022@shibaura-it.ac.jp>
2026-02-17 18:11:14 +0900

    [feature] 物理演算オブジェクトがIngameの時以外はフリーズするように

--- Assets/Scripts/Behaviour/Controller/General/SceneStateController.cs
+++ Assets/Scripts/Behaviour/Controller/General/SceneStateController.cs
@@ -1,7 +1,10 @@
 ﻿#region
 
+using System;
+using System.Collections.Generic;
 using Lib.State.Scene;
 using UnityEngine;
+using UnityEngine.Events;
 
 #endregion
 
@@ -23,6 +26,8 @@ namespace Behaviour.Controller.General
         private SceneState initialState;
 
         private ISceneState _prev;
+        
+        private Dictionary<SceneState, List<UnityAction>> _onSceneStateChanged;
 
         #region Unity Methods
 
@@ -58,6 +63,15 @@ namespace Behaviour.Controller.General
             _prev = Context.CurrentState;
             // 状態を変更する
             Context.Change(SceneStateUtils.GenerateState(next), forceChange);
+            
+            // 状態変更イベントを呼び出す
+            if (_onSceneStateChanged != null && _onSceneStateChanged.TryGetValue(next, out var actions))
+            {
+                foreach (var action in actions)
+                {
+                    action.Invoke();
+                }
+            }
         }
 
         /// <summary>
@@ -67,6 +81,25 @@ namespace Behaviour.Controller.General
         {
             Context ??= new SceneStateContext(SceneStateUtils.GenerateState(initialState));
             Context.Change(_prev, forceChange);
+        }
+        
+        /// <summary>
+        /// 特定のシーン状態に遷移したときのイベントを追加する。
+        /// </summary>
+        /// <param name="sceneState"></param>
+        /// <param name="action"></param>
+        public void AddOnSceneStateChanged(SceneState sceneState, UnityAction action)
+        {
+            _onSceneStateChanged ??= new Dictionary<SceneState, List<UnityAction>>();
+
+            if (_onSceneStateChanged.ContainsKey(sceneState))
+            {
+                _onSceneStateChanged[sceneState].Add(action);
+            }
+            else
+            {
+                _onSceneStateChanged.Add(sceneState, new List<UnityAction> { action });
+            }
         }
 
         #endregion
