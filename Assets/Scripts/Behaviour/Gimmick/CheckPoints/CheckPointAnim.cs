be89085736f1cf63ccf39c67a6e919531c66ce47
mitunaga870 <af21022@shibaura-it.ac.jp>
2026-03-06 23:47:54 +0900

    [feature] チェックポイント昨日を実装

---
+++ Assets/Scripts/Behaviour/Gimmick/CheckPoints/CheckPointAnim.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+namespace Behaviour.Gimmick.CheckPoints
+{
+    /// <summary>
+    /// チェックポイントのアニメーションのみを管理するクラス
+    /// </summary>
+    [RequireComponent(typeof(CheckPoint))]
+    public class CheckPointAnim : MonoBehaviour
+    {
+        [SerializeField]
+        private Renderer colorBoxRenderer;
+
+        private static readonly Color ActiveColor = Color.green;
+        private static readonly Color InactiveColor = Color.red;
+        
+        private void Start()
+        {
+            SetInactive();
+        }
+        
+        public void SetActive()
+        {
+            if (colorBoxRenderer == null) return;
+                
+            colorBoxRenderer.material.color = ActiveColor;
+        }
+
+        public void SetInactive()
+        {
+            if (colorBoxRenderer == null) return;
+
+            colorBoxRenderer.material.color = InactiveColor;
+        }
+    }
+}
